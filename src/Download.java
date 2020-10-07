33895523801ee9b5cd32054ed58c6ac6a860ecb9
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-10-08 01:17:43 +0300

    Add functionality with queue

--- src/Download.java
+++ src/Download.java
@@ -3,9 +3,9 @@ import model.DownloadResult;
 import java.util.Random;
 
 public class Download {
+    private static final ThreadLocal<Random> r = ThreadLocal.withInitial(Random::new);
     public DownloadResult downloadNext(int id) throws InterruptedException {
-        Random r = new Random();
-        Thread.sleep(r.nextInt(50)); //download process
-        return new DownloadResult(id, r.nextInt(100000));
+        Thread.sleep(r.get().nextInt(50)); //download process
+        return new DownloadResult(id, r.get().nextInt(100000));
     }
 }
