3a58f5a749dbc3ea155e8b4454fa19c84b662616
Hardelele <hardelele@yahoo.com>
2020-10-31 15:03:08 +0300

    Completable implementation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import model.CalculateResult;
 import model.DownloadResult;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,10 +13,12 @@ public class Main {
 
         Calendar start = Calendar.getInstance();
 
+        List<CompletableFuture<AtomicInteger>> completableFutures = new ArrayList<>();
+
         AtomicInteger finishCounter = new AtomicInteger(0);
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 3000; i++) {
             final int index = i;
-            CompletableFuture.runAsync(() -> {
+            completableFutures.add(CompletableFuture.supplyAsync(() -> {
                 Download d = new Download(index);
                 DownloadResult downloadResult = null;
                 try {
@@ -22,15 +26,19 @@ public class Main {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
+                return downloadResult;
+            }).thenApplyAsync(downloadResult -> {
                 Calculate c = new Calculate(downloadResult);
                 CalculateResult calculateResult = c.calculate();
 
                 if (calculateResult.found) {
                     finishCounter.incrementAndGet();
                 }
-            });
+                return finishCounter;
+            }));
         }
+
+        completableFutures.forEach(CompletableFuture::join);
 
         System.out.println("Total success checks: " + finishCounter);
 
