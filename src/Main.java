d6f0e4bc894759a3912d6cec7948fec3f039473a
Mikhail <livestrong174@gmail.com>
2020-11-02 22:47:59 +0500

    *Add BigTask-solution

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 import model.CalculateResult;
-import model.DownloadResult;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         Calendar start = Calendar.getInstance();
 
         int finishCounter = 0;
-        for (int i = 0; i < 1000; i++) {
 
-            Download d = new Download(i);
-            DownloadResult downloadResult = d.downloadNext();
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        List<Future<CalculateResult>> calcResults = new ArrayList<>();
 
-            Calculate c = new Calculate(downloadResult);
-            CalculateResult calculateResult = c.calculate();
+        for (int i = 0; i < 100; i++) {
+            Callable<CalculateResult> bigTask = new BigTask(i);
+            Future<CalculateResult> futureResult = service.submit(bigTask);
+            calcResults.add(futureResult);
+        }
+        service.shutdown();
 
-            if (calculateResult.found) {
+        for (Future<CalculateResult> result : calcResults) {
+            if (result.get().found) {
                 finishCounter++;
             }
         }
