722c1ae31da438ddaa92d8be5d0a340566c18134
Ivan <ivan.filonenko@gmail.com>
2021-02-15 00:58:00 +0300

    Изменено на потоки с completableFuture, выполняется за 2c

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,50 @@
 import model.CalculateResult;
-import model.DownloadResult;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	public static void main(String[] args) {
 
 		Calendar start = Calendar.getInstance();
 
-		int finishCounter = 0;
 		int TASK_NUMBER = 1000;
 
 		ExecutorService executorDownload = Executors.newFixedThreadPool(10);
 		ExecutorService executorCalculate = Executors.newFixedThreadPool(20);
-		List<Future<DownloadResult>> futures = new ArrayList<>(TASK_NUMBER);
-		List<Future<CalculateResult>> futuresCalc = new ArrayList<>(TASK_NUMBER);
 
 
-		IntStream.range(0, TASK_NUMBER).forEach(i -> futures.add(executorDownload.submit(new Download(i))));
+		List<CompletableFuture<CalculateResult>> calculateResult = IntStream.range(0, TASK_NUMBER)
+				.mapToObj(i -> CompletableFuture.supplyAsync(() -> new Download(i), executorDownload)
+						.thenApplyAsync(Main::calculateResultFuture, executorCalculate))
+				.collect(Collectors.toList());
+
+		long count = calculateResult.stream().filter(Main::isFutureCalculated).count();
+
 		executorDownload.shutdown();
-
-		for (Future<DownloadResult> f : futures) {
-			futuresCalc.add(executorCalculate.submit(new Calculate(f.get())));
-		}
 		executorCalculate.shutdown();
 
-		for (Future<CalculateResult> f : futuresCalc) {
-			if (f.get().found) {
-				finishCounter++;
-			}
-		}
-
-
-		System.out.println("Total success checks: " + finishCounter);
+		System.out.println("Total success checks: " + count);
 
 		Calendar stop = Calendar.getInstance();
 
 		System.out.println("Total time: " + (stop.getTimeInMillis() - start.getTimeInMillis()) + " ms");
 
+	}
+
+	public static CalculateResult calculateResultFuture(Download download) {
+		return new Calculate((download.call())).call();
+	}
+
+	public static boolean isFutureCalculated(Future<CalculateResult> future) {
+		try {
+			return future.get().found;
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
